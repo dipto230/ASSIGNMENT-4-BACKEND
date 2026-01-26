080a0e6fcaa130ab5a9b9a778a4b8201fd25abf2
dipto230 <diptoroy230305@gmail.com>
2026-01-27 01:30:07 +0530

    router section done

--- src/modules/public/public.routes.ts
+++ src/modules/public/public.routes.ts
@@ -6,3 +6,8 @@ const router: Router = express.Router();
 router.get("/medicines", PublicController.getMedicines);
 router.get("/medicines/:id", PublicController.getMedicineById);
 
+router.get("/categories", PublicController.getCategories);
+
+export const PublicRouter = router;
+
+
