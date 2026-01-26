0152b33e05a7ef1c64c34fa8db387f2e79f8aae8
dipto230 <diptoroy230305@gmail.com>
2026-01-27 02:04:39 +0530

    coustomer routes section done

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { SellerRouter } from "./modules/seller/seller.routes";
 import { auth } from "./lib/auth";
 import { AdminRouter } from "./modules/admin/admin.routes";
 import { PublicRouter } from "./modules/public/public.routes";
+import { CustomerRouter } from "./customer/customer.routes";
 
 
 
@@ -15,6 +16,7 @@ app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use("/api/seller", SellerRouter);
 app.use("/api/admin", AdminRouter);
 app.use("/api", PublicRouter);
+app.use("/api/customer", CustomerRouter);
  
 
 app.get("/", (req, res) => {
---
+++ src/customer/customer.routes.ts
@@ -0,0 +1,25 @@
+import express, { Router } from "express";
+import { CustomerController } from "./customer.controller";
+import { verifyAuth } from "../middlewares/auth.middleware";
+
+
+const router: Router = express.Router();
+
+
+router.use(verifyAuth);
+
+
+router.get("/cart", CustomerController.getCart);
+router.post("/cart", CustomerController.addToCart);
+router.patch("/cart/:cartItemId", CustomerController.updateCartItem);
+router.delete("/cart/:cartItemId", CustomerController.removeCartItem);
+
+
+router.post("/orders", CustomerController.placeOrder);
+router.get("/orders", CustomerController.getOrders);
+router.get("/orders/:id", CustomerController.getOrderById);
+
+
+router.post("/reviews", CustomerController.addReview);
+
+export const CustomerRouter = router;
