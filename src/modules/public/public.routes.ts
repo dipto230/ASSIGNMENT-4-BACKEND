03a2d4f9f789a918b09ef5c13630054273eeff7a
dipto230 <diptoroy230305@gmail.com>
2026-01-27 01:29:36 +0530

    adding router on routes section

--- src/modules/public/public.routes.ts
+++ src/modules/public/public.routes.ts
@@ -2,3 +2,7 @@ import express, { Router } from "express";
 import { PublicController } from "./public.controller";
 
 const router: Router = express.Router();
+
+router.get("/medicines", PublicController.getMedicines);
+router.get("/medicines/:id", PublicController.getMedicineById);
+
