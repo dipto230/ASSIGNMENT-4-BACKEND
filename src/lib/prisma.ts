f07fc663070430b2f27ffc85f4c625097fda1659
dipto230 <diptoroy230305@gmail.com>
2026-01-31 23:01:20 +0530

    redeploy

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,18 @@
-import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
+const connectionString = process.env.DATABASE_URL!;
 
-const connectionString = `${process.env.DATABASE_URL}`
+const adapter = new PrismaPg({ connectionString });
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({ adapter });
 
-export { prisma }
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
