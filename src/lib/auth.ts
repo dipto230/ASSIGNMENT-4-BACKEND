11b2aef137f0d66b421e5a1e7b72e37110903999
dipto230 <diptoroy230305@gmail.com>
2026-02-02 20:15:07 +0530

    redeploy

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,54 +1,32 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { prisma } from "./prisma";
-
+import prisma from "./prisma";
 
 export const auth = betterAuth({
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:5000",
+  baseURL: "https://medistore-assignment-70.vercel.app",
 
-  database: prismaAdapter(prisma, { provider: "postgresql" }),
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
 
-trustedOrigins: [
-  process.env.FRONTEND_URL!,
-  "http://localhost:3000",
-],
+  trustedOrigins: [
+    "https://medistore-client-side.vercel.app",
+    "http://localhost:3000",
+  ],
 
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+    requireEmailVerification: false,
+  },
 
-  // basePath: "/api/auth",
-
-  user: {
-    additionalFields: {
-      role: { type: "string", defaultValue: "CUSTOMER", required: false },
-      phone: { type: "string", required: false },
-      status: { type: "string", defaultValue: "ACTIVE", required: false },
+  advanced: {
+    defaultCookieAttributes: {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      path: "/",
+      domain: ".vercel.app",
     },
   },
-
-  emailAndPassword: { enabled: true, autoSignIn: true, requireEmailVerification: false },
-
-  // session: {
-  //   cookieCache: { enabled: true, maxAge: 7 * 24 * 60 * 60 }, 
-  //   cookieOptions: {
-  //     httpOnly: true,
-  //     secure: true,       
-  //     sameSite: "none",
-  //     path: "/",
-  //   },
-  // },
-
-  // advanced: {
-  //   cookiePrefix: "better-auth",
-  //   useSecureCookies: true,
-  //   crossSubDomainCookies: { enabled: true },
-  //   disableCSRFCheck: true,
-  // },
-advanced: {
-  defaultCookieAttributes: {
-    sameSite: "none",
-    secure: true,
-    httpOnly: true,
-    path: "/",
-  },
-},
-
 });
